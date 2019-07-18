b339a74e7be700da1a6cbfc183c25fead7d5bdc2
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-07-19 06:34:44 +0900

    Initial commit

---
+++ cmd/server/grpc.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"time"
+
+	"github.com/grpc-ecosystem/go-grpc-middleware"
+	"github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
+	"github.com/grpc-ecosystem/go-grpc-middleware/validator"
+	"github.com/infobloxopen/atlas-app-toolkit/gateway"
+	"github.com/infobloxopen/atlas-app-toolkit/requestid"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"github.com/tcvem/backend/pkg/pb"
+	"github.com/tcvem/backend/pkg/svc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+)
+
+func NewGRPCServer(logger *logrus.Logger, dbConnectionString string) (*grpc.Server, error) {
+	grpcServer := grpc.NewServer(
+		grpc.KeepaliveParams(
+			keepalive.ServerParameters{
+				Time:    time.Duration(viper.GetInt("config.keepalive.time")) * time.Second,
+				Timeout: time.Duration(viper.GetInt("config.keepalive.timeout")) * time.Second,
+			},
+		),
+		grpc.UnaryInterceptor(
+			grpc_middleware.ChainUnaryServer(
+				// logging middleware
+				grpc_logrus.UnaryServerInterceptor(logrus.NewEntry(logger)),
+
+				// Request-Id interceptor
+				requestid.UnaryServerInterceptor(),
+
+				// validation middleware
+				grpc_validator.UnaryServerInterceptor(),
+
+				// collection operators middleware
+				gateway.UnaryServerInterceptor(),
+			),
+		),
+	)
+
+	// create new postgres database
+	db, err := gorm.Open("postgres", dbConnectionString)
+	if err != nil {
+		return nil, err
+	}
+	// register service implementation with the grpcServer
+	s, err := svc.NewBasicServer(db)
+	if err != nil {
+		return nil, err
+	}
+	pb.RegisterBackendServer(grpcServer, s)
+
+	return grpcServer, nil
+}
